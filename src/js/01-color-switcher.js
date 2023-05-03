b6e772bcafb7b4e1b347d0e8f3c331284b4d7541
Yana <yana2393@gmail.com>
2023-05-03 21:08:36 +0300

    task 1-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+
+const body = document.body;
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+
+buttonStart.addEventListener('click', () => {
+    toggleDisableBtn(true);
+    body.style.backgroundColor = getRandomHexColor();
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+})
+
+buttonStop.addEventListener("click", () => {
+  toggleDisableBtn(false);
+  clearInterval(timerId);
+});
+
+function toggleDisableBtn(state) {
+    buttonStart.disabled = state;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,56 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const inputDatetimePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
+const defaultDate = new Date();
+let selectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate,
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        selectedDate = selectedDates[0];
+        if (selectedDate < defaultDate) {
+            window.alert("Please choose a date in the future");
+            startBtn.disabled = true;
+        } else {
+            startBtn.disabled = false;
+        }
+    },
+    
+};
+
+flatpickr("#datetime-picker", options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+startBtn.addEventListener('click', () => {
+    setInterval(() => {
+        const today = new Date();
+        const ms = selectedDate - today;
+        const result = convertMs(ms);
+        days.textContent = result.days;
+        hours.textContent = result.hours;
+        minutes.textContent = result.minutes;
+        seconds.textContent = result.seconds;
+    }, 1000)
+});
